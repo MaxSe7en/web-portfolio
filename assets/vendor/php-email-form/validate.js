1791662afbe7c26cd94f0dfa06bceadb0ab414ca
Maxwell Awuah Twerefour <nanayawfixing@gmail.com>
2022-04-20 07:31:32 +0000

    fixed some bugs

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -8,77 +8,96 @@
 
   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
-
-      let thisForm = this;
-
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
-
-      let formData = new FormData( thisForm );
-
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
+  forms.forEach(function (e) {
+    e.addEventListener('submit', sendContact);
   });
 
   function php_email_form_submit(thisForm, action, formData) {
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
+      .then(response => {
+        if (response.ok) {
+          return response.text()
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (data.trim() == 'OK') {
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        } else {
+          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
   }
 
-  function displayError(thisForm, error) {
+  // const contact = document.getElementById("submit");
+
+
+  function resetInputs() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+  }
+
+  function sendContact(evt) {
+    evt.preventDefault();
+
+    let thisForm = this;
+
+
+
+    thisForm.querySelector('.loading').classList.add('d-block');
+    thisForm.querySelector('.error-message').classList.remove('d-block');
+    thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const _message = document.getElementById("message").value;
+    console.log(`this is contact`)
+    const body = `${_message}
+    **sent by**: ${name}
+    `
+    Email.send({
+      SecureToken: "edc7ed75-0ad6-4caf-8e1f-244dba901168",
+      Host: "smtp.gmail.com",
+      Username: "nanayawfixing@gmail.com",
+      Password: "*******",
+      To: 'nanayawfixing@gmail.com',
+      From: email,
+      Subject: subject,
+      Body: body
+    }).then(
+      (message) => {
+        if (message === "OK") {
+          thisForm.querySelector('.loading').classList.remove('d-block');
+          resetInputs();
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          // alert("We have received your message, Thank you!");
+        } else {
+          // alert("Your message was not sent, check your internet connection");
+          console.log(`this is the message ${message}`)
+          displayError(thisForm)
+        }
+      }
+    );
+  }
+
+  // contact.addEventListener("submit", sendContact);
+
+  function displayError(thisForm) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').innerText = 'Your message was not sent, check your internet connection';
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
